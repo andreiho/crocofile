486a894f9a6ab7da3b4426ef22d0bf7e2c03eea4
Andrei Horodinca <andrei.horodinca@gmail.com>
2015-06-01 11:29:13 +0200

    Add semantic ui library

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, render_template
 from flask.ext.bower import Bower
+
+SECRET_KEY = "lol"
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
