46d0fa87b43de5add109435bf25f8a85e019b66c
Andrei Horodinca <andrei.horodinca@gmail.com>
2015-06-02 17:09:53 +0200

    Set timeout on alerts

--- static/main.js
+++ static/main.js
@@ -3,3 +3,8 @@ $('.message .close').on('click', function() {
   console.log("lalala");
   $(this).closest('.message').fadeOut();
 });
+
+// Close alerts after some seconds.
+setTimeout(function () {
+  $('.message').fadeOut();
+}, 5000);
