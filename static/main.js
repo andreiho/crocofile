76bffcf017cfad13747c849790a754ca9dd61a4d
jagenau <sarah@radicalpet.net>
2015-06-04 14:24:43 +0200

    something something encryption, decryption

--- static/main.js
+++ static/main.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
 	  $('.message').fadeOut();
 	}, 5000);
 
-
 	// some shit from google code, needs to be in different file
 
 	CryptoJS.enc.u8array = {
@@ -102,7 +101,7 @@ $(document).ready(function(){
 			reader.onload = function(e) {
 
 			  	var ui8a = new Uint8Array(reader.result);
-				var wordarray = CryptoJS.enc.u8array.parse(encryptedSlices[0]);
+				var wordarray = CryptoJS.enc.u8array.parse(ui8a);
 				console.log("plaintext word array: ");
 				console.log(wordarray);
 
@@ -115,13 +114,7 @@ $(document).ready(function(){
 				if (i == slices.length) {
 
 					encryptedSlices.push(aesEncryptor.finalize());
-					console.log("ciphertext 1: ");
-					
-					var wordarray = CryptoJS.enc.u8array.parse(encryptedSlices[0]);
-					console.log(ui8a);
-					console.log("ciphertext 2: ");
-					console.log(encryptedSlices[1]);
-
+			
 					decrypt(encryptedSlices[0], encryptedSlices[1], passphrase, iv);
 					
 					$.each(encryptedSlices, function(index, value) {
@@ -170,6 +163,7 @@ $(document).ready(function(){
 
 		var slices = sliceFile(file);
 		var passphrase = $('#passphrase').val();
+		passphrase =  CryptoJS.enc.Hex.parse(passphrase);
 		var iv = createIV();
 		console.log("iv: " + iv);
 		console.log("passphrase: " + passphrase);
@@ -212,18 +206,102 @@ $(document).ready(function(){
 	    }    
 	    return result;
 	}
-	function decrypt(text1, text2, iv, passphrase) {
-		var aesDecryptor = CryptoJS.algo.AES.createDecryptor(passphrase, { iv: iv });
-		console.log("ciphertext 1 in decrypt: ");
-		console.log(text1);
-    	var plaintextPart1 = aesDecryptor.process(CryptoJS.enc.u8array.parse(text1));
-    	
-    	console.log("plaintext 1: " + plaintextPart1);
-
-    	var plaintextPart2 = aesDecryptor.process(CryptoJS.enc.u8array.parse(text2));
-    	
-    	console.log("plaintext 2: " + plaintextPart2);
+/*
+wordArray: { words: [..], sigBytes: words.length * 4 }
+*/
+ 
+// assumes wordArray is Big-Endian (because it comes from CryptoJS which is all BE)
+// From: https://gist.github.com/creationix/07856504cf4d5cede5f9#file-encode-js
+function convertWordArrayToUint8Array(wordArray) {
+	var len = wordArray.words.length,
+		u8_array = new Uint8Array(len << 2),
+		offset = 0, word, i
+	;
+	for (i=0; i<len; i++) {
+		word = wordArray.words[i];
+		u8_array[offset++] = word >> 24;
+		u8_array[offset++] = (word >> 16) & 0xff;
+		u8_array[offset++] = (word >> 8) & 0xff;
+		u8_array[offset++] = word & 0xff;
 	}
+	return u8_array;
+}
+ 
+// create a wordArray that is Big-Endian (because it's used with CryptoJS which is all BE)
+// From: https://gist.github.com/creationix/07856504cf4d5cede5f9#file-encode-js
+function convertUint8ArrayToWordArray(u8Array) {
+	var words = [], i = 0, len = u8Array.length;
+ 
+	while (i < len) {
+		words.push(
+			(u8Array[i++] << 24) |
+			(u8Array[i++] << 16) |
+			(u8Array[i++] << 8)  |
+			(u8Array[i++])
+		);
+	}
+ 
+	return {
+		sigBytes: words.length * 4,
+		words: words
+	};
+}
+ 
+function convertUint8ArrayToBinaryString(u8Array) {
+	var i, len = u8Array.length, b_str = "";
+	for (i=0; i<len; i++) {
+		b_str += String.fromCharCode(u8Array[i]);
+	}
+	return b_str;
+}
+ 
+function convertBinaryStringToUint8Array(bStr) {
+	var i, len = bStr.length, u8_array = new Uint8Array(len);
+	for (var i = 0; i < len; i++) {
+		u8_array[i] = bStr.charCodeAt(i);
+	}
+	return u8_array;
+}
+function decrypt(text1, text2, iv, passphrase) {
+	var aesDecryptor = CryptoJS.algo.AES.createDecryptor(passphrase, { iv: iv });
+	var decryptedSlices = [];
+	decryptedSlices.push(aesDecryptor.process(text1));
+	decryptedSlices.push(aesDecryptor.process(text2));
+	decryptedSlices.push(aesDecryptor.finalize());
+
+	console.log(decryptedSlices.length);
+
+	setTimeout(function() {
+		$.each(decryptedSlices, function(index, value) {
+
+			$.ajax({
+		      url: '/upload',
+		      type: 'POST',
+		      xhr: function() { // custom xhr
+		        var myXhr = $.ajaxSettings.xhr();
+		        if(myXhr.upload) { // check if upload property exists
+		          myXhr.upload.addEventListener('progress', progressHandler, false); // for handling the progress of the upload
+		        }
+		        return myXhr;
+		      },
+		      contentType: 'text/plain',
+		      //Ajax events
+		      //beforeSend: beforeSendHandler,
+		      success: successHandler,
+		      error: errorHandler,
+		      // Form data
+		      headers: {
+		        'X-File-Content-Type': "application/octet-stream",
+		        'X-File-Name': "decrypted",
+		        'X-Chunk-Number': index
+		      },
+		      data: value, // binary chunk
+		      processData: false,
+		      cache: false
+		    });
+		});
+	}, 1000);
+}
 
 	$('#passphrase').val(generatePass()); 
 	$('#generate').on('click', newPass);
